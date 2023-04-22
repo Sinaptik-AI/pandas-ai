c31ba21be4c7499aed388bd0f0eeb64de1705c0e
Gabriele Venturi <lele.venturi@gmail.com>
2023-04-23 00:24:24 +0200

    Add example main.py file

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pandas_ai import PandasAI
+from llm.openai import OpenAI
+
+df = pd.DataFrame({
+    "country": ["United States", "United Kingdom", "France", "Germany", "Italy", "Spain", "Canada", "Australia", "Japan", "China"],
+    "gdp": [21400000, 2940000, 2830000, 3870000, 2160000, 1350000, 1780000, 1320000, 516000, 14000000],
+    "happiness_index": [7.3, 7.2, 6.5, 7.0, 6.0, 6.3, 7.3, 7.3, 5.9, 5.0]
+})
+
+llm = OpenAI()
+pandas_ai = PandasAI(llm)
+pandas_ai.run(df, "Calculate the sum of the gdp of north american countries")
