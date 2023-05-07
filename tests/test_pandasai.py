10b6dc62511ce5bd4a928b36b9f13c58e617782b
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-07 20:16:30 -0300

    [tests] replacing repetition of setup() with pytest fixtures

--- tests/test_pandasai.py
+++ tests/test_pandasai.py
@@ -17,122 +17,106 @@ class TestPandasAI:
 
     # pylint: disable=missing-function-docstring protected-access invalid-name
 
-    llm: FakeLLM
-    pandasai: PandasAI
+    @pytest.fixture
+    def llm(self, output: Optional[str] = None):
+        return FakeLLM(output=output)
 
-    def setup(self, output: Optional[str] = None):
-        self.llm = FakeLLM(output=output)
-        self.pandasai = PandasAI(self.llm)
+    @pytest.fixture
+    def pandasai(self, llm):
+        return PandasAI(llm)
 
-    def test_init(self):
-        self.setup()
-        assert self.pandasai._llm == self.llm
-        assert self.pandasai._is_conversational_answer is True
-        assert self.pandasai._verbose is False
-
-    def test_init_with_llm(self):
-        self.setup()
-        assert self.pandasai._llm == self.llm
-        assert self.pandasai._is_conversational_answer is True
-        assert self.pandasai._verbose is False
+    def test_init(self, pandasai):
+        assert pandasai._llm is not None
+        assert pandasai._is_conversational_answer is True
+        assert pandasai._verbose is False
 
     def test_init_without_llm(self):
         with pytest.raises(LLMNotFoundError):
             PandasAI()
 
-    def test_conversational_answer(self):
+    def test_conversational_answer(self, pandasai, llm):
         result = "2"
-        self.setup(result)
+        llm._output = result
         assert (
-            self.pandasai.conversational_answer("What is the sum of 1 + 1?", "1 + 1", 2)
+            pandasai.conversational_answer("What is the sum of 1 + 1?", "1 + 1", 2)
             == result
         )
 
-    def test_run(self):
+    def test_run(self, pandasai, llm):
         df = pd.DataFrame()
-        self.setup(output="1")
-        assert self.pandasai.run(df, "What number comes before 2?") == "1"
+        llm._output = "1"
+        assert pandasai.run(df, "What number comes before 2?") == "1"
 
-    def test_run_with_conversational_answer(self):
+    def test_run_with_conversational_answer(self, pandasai, llm):
         df = pd.DataFrame()
-        self.setup(output="1 + 1")
+        llm._output = "1 + 1"
         assert (
-            self.pandasai.run(
-                df, "What is the sum of 1 + 1?", is_conversational_answer=True
-            )
+            pandasai.run(df, "What is the sum of 1 + 1?", is_conversational_answer=True)
             == "1 + 1"
         )
 
-    def test_run_with_non_conversational_answer(self):
+    def test_run_with_non_conversational_answer(self, pandasai, llm):
         df = pd.DataFrame()
-        self.setup(output="1 + 1")
+        llm._output = "1 + 1"
         assert (
-            self.pandasai.run(
+            pandasai.run(
                 df, "What is the sum of 1 + 1?", is_conversational_answer=False
             )
             == 2
         )
 
-    def test_run_with_verbose(self):
+    def test_run_with_verbose(self, pandasai, llm):
         df = pd.DataFrame()
-        self.setup(output="1")
-        self.pandasai._verbose = True
+        pandasai._verbose = True
 
         # mock print function
         with patch("builtins.print") as mock_print:
-            self.pandasai.run(df, "What number comes before 2?")
+            pandasai.run(df, "What number comes before 2?")
             mock_print.assert_called()
 
-    def test_run_without_verbose(self):
+    def test_run_without_verbose(self, pandasai, llm):
         df = pd.DataFrame()
-        self.setup(output="1")
-        self.pandasai._verbose = False
+        pandasai._verbose = False
+        llm._output = "1"
 
         # mock print function
         with patch("builtins.print") as mock_print:
-            self.pandasai.run(df, "What number comes before 2?")
+            pandasai.run(df, "What number comes before 2?")
             mock_print.assert_not_called()
 
-    def test_run_code(self):
+    def test_run_code(self, llm):
         df = pd.DataFrame()
-        self.setup()
-        assert self.pandasai.run_code("1 + 1", df) == 2
+        pandasai = PandasAI(llm=llm)
+        assert pandasai.run_code("1 + 1", df) == 2
 
     def test_run_code_invalid_code(self):
         df = pd.DataFrame()
-        self.setup()
         with pytest.raises(Exception):
-            self.pandasai.run_code("1 +", df, use_error_correction_framework=False)
+            PandasAI().run_code("1 +", df, use_error_correction_framework=False)
 
-    def test_run_code_with_print(self):
+    def test_run_code_with_print(self, pandasai):
         df = pd.DataFrame()
-        self.setup()
-        assert self.pandasai.run_code("print(1 + 1)", df) == 2
+        assert pandasai.run_code("print(1 + 1)", df) == 2
 
-    def test_conversational_answer_with_privacy_enforcement(self):
-        self.setup()
-        self.pandasai._enforce_privacy = True
-        self.pandasai._llm.call = Mock(return_value="The answer is 2")
-        assert (
-            self.pandasai.conversational_answer("How much does 1 + 1 do?", "", 2) == 2
-        )
-        self.pandasai._llm.call.assert_not_called()
+    def test_conversational_answer_with_privacy_enforcement(self, pandasai, llm):
+        pandasai._enforce_privacy = True
+        llm.call = Mock(return_value="The answer is 2")
+        assert pandasai.conversational_answer("How much does 1 + 1 do?", "", 2) == 2
+        llm.call.assert_not_called()
 
-    def test_conversational_answer_without_privacy_enforcement(self):
-        self.setup()
-        self.pandasai._enforce_privacy = False
-        self.pandasai._llm.call = Mock(return_value="The answer is 2")
+    def test_conversational_answer_without_privacy_enforcement(self, pandasai, llm):
+        pandasai._enforce_privacy = False
+        llm.call = Mock(return_value="The answer is 2")
         assert (
-            self.pandasai.conversational_answer("How much does 1 + 1 do?", "", 2)
+            pandasai.conversational_answer("How much does 1 + 1 do?", "", 2)
             == "The answer is 2"
         )
-        self.pandasai._llm.call.assert_called()
+        llm.call.assert_called()
 
-    def test_run_with_privacy_enforcement(self):
+    def test_run_with_privacy_enforcement(self, pandasai, llm):
         df = pd.DataFrame({"country": ["United States", "United Kingdom", "France"]})
-        self.setup()
-        self.pandasai._enforce_privacy = True
-        self.pandasai._is_conversational_answer = True
+        pandasai._enforce_privacy = True
+        pandasai._is_conversational_answer = True
 
         expected_prompt = f"""
 Today is {date.today()}.
@@ -147,10 +131,10 @@ How many countries are in the dataframe?
 
 Code:
 """
-        self.pandasai.run(df, "How many countries are in the dataframe?")
-        assert self.pandasai._llm.last_prompt == expected_prompt
+        pandasai.run(df, "How many countries are in the dataframe?")
+        assert pandasai._llm.last_prompt == expected_prompt
 
-    def test_run_with_anonymized_df(self):
+    def test_run_with_anonymized_df(self, pandasai):
         df = pd.DataFrame(
             {
                 "Phone Number": ["(743) 226-2382", "+1 123456789", "0002223334"],
@@ -176,9 +160,7 @@ Code:
                 ],
             }
         )
-        self.setup()
-        self.pandasai._is_conversational_answer = True
-
+        pandasai._is_conversational_answer = True
         expected_non_anonymized_data_frame_substring = """
 This is the result of `print(df.head(5))`:
      Phone Number                          Email             Name  Age   Credit Card Number
@@ -186,19 +168,16 @@ This is the result of `print(df.head(5))`:
 1    +1 123456789   oliverdouglas@lee-harris.biz  Nathan Richards   21  3109-2849-2297-7926
 2      0002223334  sara41@mitchell-rodriguez.com     Monica Scott   27  4795 0612 5882 4558.
 """
-        self.pandasai.run(
-            df, "How many people are in the dataframe?", anonymize_df=True
-        )
+        pandasai.run(df, "How many people are in the dataframe?", anonymize_df=True)
         assert (
             expected_non_anonymized_data_frame_substring
-            not in self.pandasai._llm.last_prompt
+            not in pandasai._llm.last_prompt
         )
 
-    def test_run_without_privacy_enforcement(self):
+    def test_run_without_privacy_enforcement(self, pandasai):
         df = pd.DataFrame({"country": ["United States", "United Kingdom", "France"]})
-        self.setup()
-        self.pandasai._enforce_privacy = False
-        self.pandasai._is_conversational_answer = False
+        pandasai._enforce_privacy = False
+        pandasai._is_conversational_answer = False
 
         expected_prompt = f"""
 Today is {date.today()}.
@@ -214,52 +193,48 @@ How many countries are in the dataframe?
 
 Code:
 """
-        self.pandasai.run(
-            df, "How many countries are in the dataframe?", anonymize_df=False
-        )
-        assert self.pandasai._llm.last_prompt == expected_prompt
+        pandasai.run(df, "How many countries are in the dataframe?", anonymize_df=False)
+        assert pandasai._llm.last_prompt == expected_prompt
 
-    def test_run_with_print_at_the_end(self):
+    def test_run_with_print_at_the_end(self, pandasai, llm):
         code = """
 result = {'happiness': 0.5, 'gdp': 0.8}
 print(result)"""
-        self.setup(code)
-        self.pandasai.run_code(code, pd.DataFrame())
+        llm._output = code
+        pandasai.run_code(code, pd.DataFrame())
 
-    def test_extract_code(self):
-        self.setup()
-
+    def test_extract_code(self, pandasai):
         code = """```python
 result = {'happiness': 0.5, 'gdp': 0.8}
 print(result)```"""
         assert (
-            self.pandasai._llm._extract_code(code)
+            pandasai._llm._extract_code(code)
             == "result = {'happiness': 0.5, 'gdp': 0.8}\nprint(result)"
         )
 
         code = """```
 result = {"happiness": 1, "gdp": 0.43}```"""
         assert (
-            self.pandasai._llm._extract_code(code)
+            pandasai._llm._extract_code(code)
             == "result = {'happiness': 1, 'gdp': 0.43}"
         )
 
         code = """```python<startCode>
 result = {'happiness': 0.3, 'gdp': 5.5}<endCode>```"""
         assert (
-            self.pandasai._llm._extract_code(code)
+            pandasai._llm._extract_code(code)
             == "result = {'happiness': 0.3, 'gdp': 5.5}"
         )
 
         code = """<startCode>```python
 result = {'happiness': 0.49, 'gdp': 25.5}```<endCode>"""
         assert (
-            self.pandasai._llm._extract_code(code)
+            pandasai._llm._extract_code(code)
             == "result = {'happiness': 0.49, 'gdp': 25.5}"
         )
         code = """<startCode>```python
 result = {'happiness': 0.49, 'gdp': 25.5}```"""
         assert (
-            self.pandasai._llm._extract_code(code)
+            pandasai._llm._extract_code(code)
             == "result = {'happiness': 0.49, 'gdp': 25.5}"
         )
