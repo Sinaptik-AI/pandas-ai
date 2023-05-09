3e14ad5c3b0bacc534d3ea71419231973d358feb
yzaparto <yashzaparto3@gmail.com>
2023-05-08 20:16:09 -0700

    fix linter raised problems

--- pandasai/helpers/plot.py
+++ pandasai/helpers/plot.py
@@ -4,10 +4,22 @@ This module contains helper functions for generating plots.
 
 
 def is_code_for_plots(code: str,plt_code: str, plot_keywords: list[str]) -> bool:
+    """
+    Checks whether the code is for generating the plots.
+
+    Returns:
+        bool: True if the code is for generating the plots, False otherwise.
+    """
     no_plt = code.count(plt_code) < 1
     return any(keyword_generator(code, plot_keywords)) and no_plt
 
 
 def keyword_generator(code: str, plot_keywords: list[str]):
-    for x in plot_keywords:
-        yield x in code
+    """
+     A generator function that iterates over plot_keywords.
+
+    Returns:
+        Generator
+    """
+    for plot_keyword in plot_keywords:
+        yield plot_keyword in code
