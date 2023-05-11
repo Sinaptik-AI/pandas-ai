3782ce1bd9ba790567ff84cf6633ef3f4be7091b
Victor Del Carpio <vhdelcg@gmail.com>
2023-05-11 12:56:03 -0400

    fix: using click instead of argparse

--- pai/__main__.py
+++ pai/__main__.py
@@ -1,53 +1,46 @@
-import argparse
+import os
+from dotenv import load_dotenv
+import click
+import pandas as pd
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 from pandasai.llm.open_assistant import OpenAssistant
-import pandas as pd
-
-import os
-from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-parser = argparse.ArgumentParser(description='Pandas AI command-line tool.')
-parser.add_argument('-d', '--dataset', type=str, required=True, help='The dataset to use.')
-parser.add_argument('-t', '--token', type=str, required=False, default=None, help='The API token to use.')
-parser.add_argument('-m', '--model', type=str, required=True, help='The type of model to use.')
-parser.add_argument('-p', '--prompt', type=str, required=True, help='The prompt to use.')
-
-def main():
-    args = parser.parse_args()
-
-    ext: str = os.path.splitext(args.dataset)[1]
+@click.command()
+@click.option('-d', '--dataset', type=str, required=True, help='The dataset to use.')
+@click.option('-t', '--token', type=str, required=False, default=None, help='The API token to use.')
+@click.option('-m', '--model', type=click.Choice(['openai', 'open-assistant']), required=True, help='The type of model to use.')
+@click.option('-p', '--prompt', type=str, required=True, help='The prompt to use.')
+def main(dataset, token, model, prompt):
+    ext = os.path.splitext(dataset)[1]
 
     try:
         if ext == '.csv':
-            df = pd.read_csv(args.dataset)
+            df = pd.read_csv(dataset)
         elif ext == ".xlsx":
-            df = pd.read_excel(args.dataset)
+            df = pd.read_excel(dataset)
         elif ext == ".json":
-            df = pd.read_json(args.dataset)
+            df = pd.read_json(dataset)
         else:
-            print(f"{args.dataset} does not contain a valid file extension.")
+            print(f"{dataset} does not contain a valid file extension.")
+            return
 
     except:
-        print(f"Could not find {args.dataset}")
+        print(f"Could not find {dataset}")
         return
 
-    if args.model == "openai":
-        llm = OpenAI(api_token = args.token or os.environ.get("OPENAI_API_KEY"))
+    if model == "openai":
+        llm = OpenAI(api_token = token or os.environ.get("OPENAI_API_KEY"))
 
-    elif args.model == "open-assistant":
-        llm = OpenAssistant(api_token = args.token or os.environ.get("HUGGINGFACE_API_KEY"))
-
-    else:
-        print(f"Invalid model type: {args.model}")
-        return
+    elif model == "open-assistant":
+        llm = OpenAssistant(api_token = token or os.environ.get("HUGGINGFACE_API_KEY"))
 
     try:
         pandas_ai = PandasAI(llm, verbose=True)
-        response = pandas_ai.run(df, args.prompt)
+        response = pandas_ai.run(df, prompt)
         print(response)
 
     except:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pandasai-cli',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = ['pai'],
     entry_points = {
         'console_scripts': [
