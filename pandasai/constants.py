2f7385e8eb0e7a65415cf4abb937ac81523e364a
Saad Salman <132683924+ss9901@users.noreply.github.com>
2023-05-11 20:50:02 +0500

    Update constants.py

--- pandasai/constants.py
+++ pandasai/constants.py
@@ -2,74 +2,19 @@
 Constants used in the pandasai package.
 """
 
-START_CODE_TAG = "<startCode>"
-END_CODE_TAG = "<endCode>"
-WHITELISTED_LIBRARIES = [
-    "numpy",
-    "matplotlib",
-]
-WHITELISTED_BUILTINS = [
-    "abs",
-    "all",
-    "any",
-    "ascii",
-    "bin",
-    "bool",
-    "bytearray",
-    "bytes",
-    "callable",
-    "chr",
-    "classmethod",
-    "complex",
-    "delattr",
-    "dict",
-    "dir",
-    "divmod",
-    "enumerate",
-    "filter",
-    "float",
-    "format",
-    "frozenset",
-    "getattr",
-    "hasattr",
-    "hash",
-    "help",
-    "hex",
-    "id",
-    "input",
-    "int",
-    "isinstance",
-    "issubclass",
-    "iter",
-    "len",
-    "list",
-    "locals",
-    "map",
-    "max",
-    "memoryview",
-    "min",
-    "next",
-    "object",
-    "oct",
-    "open",
-    "ord",
-    "pow",
-    "print",
-    "property",
-    "range",
-    "repr",
-    "reversed",
-    "round",
-    "set",
-    "setattr",
-    "slice",
-    "sorted",
-    "staticmethod",
-    "str",
-    "sum",
-    "super",
-    "tuple",
-    "type",
-    "vars",
-    "zip",
-]
+# Code block tags
+START_CODE_TAG = "<start_code>"
+END_CODE_TAG = "<end_code>"
+
+# Whitelisted libraries and built-in functions
+WHITELISTED_LIBRARIES = ("numpy", "matplotlib")
+WHITELISTED_BUILTINS = (
+    "abs", "all", "any", "ascii", "bin", "bool", "bytearray", "bytes",
+    "callable", "chr", "classmethod", "complex", "delattr", "dict", "dir",
+    "divmod", "enumerate", "filter", "float", "format", "frozenset", "getattr",
+    "hasattr", "hash", "help", "hex", "id", "input", "int", "isinstance",
+    "issubclass", "iter", "len", "list", "locals", "map", "max", "memoryview",
+    "min", "next", "object", "oct", "open", "ord", "pow", "print", "property",
+    "range", "repr", "reversed", "round", "set", "setattr", "slice", "sorted",
+    "staticmethod", "str", "sum", "super", "tuple", "type", "vars", "zip"
+)
