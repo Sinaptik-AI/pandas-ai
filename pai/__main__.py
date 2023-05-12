6831ce2937d1806e42e4a22117a76ea080871db1
Victor Del Carpio <vhdelcg@gmail.com>
2023-05-12 19:25:47 -0400

    feat: support all file types for pandas

--- pai/__main__.py
+++ pai/__main__.py
@@ -16,14 +16,38 @@ def main(dataset, token, model, prompt):
     ext = os.path.splitext(dataset)[1]
 
     try:
-        if ext == '.csv':
-            df = pd.read_csv(dataset)
-        elif ext == ".xlsx":
-            df = pd.read_excel(dataset)
-        elif ext == ".json":
-            df = pd.read_json(dataset)
+        format = {
+            ".csv": pd.read_csv,
+            ".xls": pd.read_excel,
+            ".xlsx": pd.read_excel,
+            ".xlsm": pd.read_excel,
+            ".xlsb": pd.read_excel,
+            ".json": pd.read_json,
+            ".html": pd.read_html,
+            ".sql": pd.read_sql,
+            ".feather": pd.read_feather,
+            ".parquet": pd.read_parquet,
+            ".dta": pd.read_stata,
+            ".sas7bdat": pd.read_sas,
+            ".h5": pd.read_hdf,
+            ".hdf5": pd.read_hdf,
+            ".pkl": pd.read_pickle,
+            ".pickle": pd.read_pickle,
+            ".gbq": pd.read_gbq,
+            ".orc": pd.read_orc,
+            ".xpt": pd.read_sas,
+            ".sav": pd.read_spss,
+            ".gz": pd.read_csv,
+            ".zip": pd.read_csv,
+            ".bz2": pd.read_csv,
+            ".xz": pd.read_csv,
+            ".txt": pd.read_csv,
+            ".xml": pd.read_xml,
+        }
+        if ext in format:
+            df = format[ext](dataset)
         else:
-            print(f"{dataset} does not contain a valid file extension.")
+            print("Unsupported file format.")
             return
 
     except:
