a26fce5b30b7990654c252891b91a0e0f9062e92
Aryan Gandhi <aryandgandhi@gmail.com>
2023-05-18 08:11:18 -0500

    added instructions

--- examples/from_googlesheets.py
+++ examples/from_googlesheets.py
@@ -5,12 +5,13 @@ from pandasai import PandasAI
 from pandasai.helpers.datasource import sheets_input
 from pandasai.llm.openai import OpenAI
 
+# instructions: https://docs.google.com/document/d/1uAmXI3e8Wm26lZQb0bYIJaXRdZi5WLJKNJHT59QMGEc/edit?usp=sharing
 df = sheets_input(
     "1IRGJFUpCR0-9tPDk2MgPjQCtNft_4a8B1fHgYK7_LM8",
     "second",
     "../service_account_file.json",
 )
-# specify type of columns
+# specify type of columns, google sheets is not strongly typed
 df["gdp"] = pd.to_numeric(df["gdp"])
 df["happiness_index"] = pd.to_numeric(df["happiness_index"])
 
