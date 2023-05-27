3a31148533e68677e5348c913faf002e3d6cac9d
Aryan Gandhi <aryandgandhi@gmail.com>
2023-05-27 13:15:01 -0500

    testing

--- examples/from_googlesheets.py
+++ examples/from_googlesheets.py
@@ -3,19 +3,19 @@
 import pandas as pd
 
 from pandasai import PandasAI
-from pandasai.helpers.datasource import sheets_input
+from pandasai.helpers.datasource import from_google_sheets
 from pandasai.llm.openai import OpenAI
 
 # instructions:
 # https://docs.google.com/document/d/1uAmXI3e8Wm26lZQb0bYIJaXRdZi5WLJKNJHT59QMGEc/edit?usp=sharing
-df = sheets_input(
+df = from_google_sheets(
     "1IRGJFUpCR0-9tPDk2MgPjQCtNft_4a8B1fHgYK7_LM8",
     "second",
     "../service_account_file.json",
 )
+
 # specify type of columns, google sheets is not strongly typed
-df["gdp"] = pd.to_numeric(df["gdp"])
-df["happiness_index"] = pd.to_numeric(df["happiness_index"])
+
 
 llm = OpenAI()
 pandas_ai = PandasAI(llm, verbose=True, conversational=False)
@@ -25,4 +25,3 @@ response = pandas_ai.run(
     "Calculate the sum of the gdp of north american countries",
 )
 print(response)
-Output: 20901884461056
