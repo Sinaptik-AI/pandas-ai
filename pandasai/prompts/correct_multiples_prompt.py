c6576766d58221ecdbed0f44b0ac80e6d3e35d94
Gabriele Venturi <lele.venturi@gmail.com>
2023-05-28 12:54:06 +0200

    test: correct multiple dataframes

--- pandasai/prompts/correct_multiples_prompt.py
+++ pandasai/prompts/correct_multiples_prompt.py
@@ -1,24 +1,26 @@
 """ Prompt to correct error """
 
+import pandas as pd
+
 from pandasai.constants import END_CODE_TAG, START_CODE_TAG
 
 from .base import Prompt
 
-import pandas as pd
 
 class CorrectMultipleDataframesErrorPrompt(Prompt):
     """Prompt to generate Python code"""
+
     text: str = """
-You are provided with the following pandas dataframes:    
-"""
+You are provided with the following pandas dataframes:"""
 
-    def __init__(self, 
-                 code: str, 
-                 error_returned: Exception, 
-                 question: str, 
-                 df_head: list[pd.DataFrame]):
-
-        for i, dataframe in enumerate(df_head, start = 1):
+    def __init__(
+        self,
+        code: str,
+        error_returned: Exception,
+        question: str,
+        df_head: list[pd.DataFrame],
+    ):
+        for i, dataframe in enumerate(df_head, start=1):
             row, col = dataframe.shape
             self.text += f"""
 Dataframe df{i}, with {row} rows and {col} columns.
@@ -40,6 +42,6 @@ Make sure to prefix the requested python code with {START_CODE_TAG} exactly and 
 """
 
         self.text += instruction
-    
+
     def __str__(self):
         return self.text
