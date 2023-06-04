f22f5bb8799c09b017c952f65a549cfe4ee9fda8
Jonathan Biemond <101326085+jonbiemond@users.noreply.github.com>
2023-06-04 15:15:07 -0700

    chore: add save_charts documentation and format docs (#215)
    chore: fix typehints
    
    fix: only create save_chart directory if there are calls to plt.show()
    
    Co-authored-by: Gabriele Venturi <lele.venturi@gmail.com>

--- pandasai/helpers/save_chart.py
+++ pandasai/helpers/save_chart.py
@@ -14,8 +14,20 @@ def compare_ast(
     node2: Union[ast.expr, list[ast.expr], ast.stmt, ast.AST],
     ignore_args=False,
 ) -> bool:
-    """Compare two AST nodes for equality.
-    Source: https://stackoverflow.com/a/66733795/11080806"""
+    """
+    Compare two AST nodes for equality.
+    Source: https://stackoverflow.com/a/66733795/11080806
+
+    Args:
+        node1 (ast.AST): First AST node to compare.
+        node2 (ast.AST): Second AST node to compare.
+        ignore_args (bool, optional): Whether to ignore the arguments of the nodes.
+            Defaults to False.
+
+    Returns:
+        bool: True if the nodes are equal, False otherwise.
+
+    """
     if type(node1) is not type(node2):
         return False
 
@@ -38,15 +50,22 @@ def compare_ast(
 
 
 def add_save_chart(code: str) -> str:
-    """Add line to code that save charts to a file, if plt.show() is called."""
+    """
+    Add line to code that save charts to a file, if plt.show() is called.
+
+    Args:
+        code (str): Code to add line to.
+
+    Returns:
+        str: Code with line added.
+
+    """
     date = datetime.now().strftime("%Y-%m-%d")
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
     # define chart save directory
     project_root = dirname(dirname(dirname(__file__)))
     chart_save_dir = os.path.join(project_root, "exports", "charts", date)
-    if not os.path.exists(chart_save_dir):
-        os.makedirs(chart_save_dir)
 
     tree = ast.parse(code)
 
@@ -59,6 +78,9 @@ def add_save_chart(code: str) -> str:
     # if there are no plt.show() calls, return the original code
     if show_count == 0:
         return code
+
+    if not os.path.exists(chart_save_dir):
+        os.makedirs(chart_save_dir)
 
     # iterate through the AST and add plt.savefig() calls before plt.show() calls
     counter = ord("a")
