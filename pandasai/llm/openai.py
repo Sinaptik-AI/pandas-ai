07a93d99bc9b90cc0e342861b55279306f86cc63
Gabriele Venturi <lele.venturi@gmail.com>
2023-06-14 17:00:26 +0200

    feat: add support for new openai models

--- pandasai/llm/openai.py
+++ pandasai/llm/openai.py
@@ -34,11 +34,13 @@ class OpenAI(BaseOpenAI):
 
     _supported_chat_models = [
         "gpt-4",
-        "gpt-4-0314",
+        "gpt-4-0613",
         "gpt-4-32k",
-        "gpt-4-32k-0314",
+        "gpt-4-32k-0613",
         "gpt-3.5-turbo",
-        "gpt-3.5-turbo-0301",
+        "gpt-3.5-turbo-16k",
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
     ]
     _supported_completion_models = ["text-davinci-003"]
 
