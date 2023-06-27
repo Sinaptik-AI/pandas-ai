b57f1f3193343136416b580c58acc195240f91c1
Gabriele Venturi <lele.venturi@gmail.com>
2023-06-28 01:43:12 +0200

    fix: stop plot execution in non-console environment (#52)

--- pandasai/middlewares/charts.py
+++ pandasai/middlewares/charts.py
@@ -5,10 +5,21 @@ Middleware to handle the charts in PandasAI.
 """
 
 from pandasai.middlewares.base import Middleware
+import sys
 
 
 class ChartsMiddleware(Middleware):
     """Charts Middleware class"""
+
+    def _is_running_in_console(self) -> bool:
+        """
+        Check if the code is running in console or not.
+
+        Returns:
+            bool: True if running in console else False
+        """
+
+        return sys.stdout.isatty()
 
     def run(self, code: str) -> str:
         """
@@ -18,6 +29,10 @@ class ChartsMiddleware(Middleware):
             str: Modified code
         """
 
-        if "plt.show()" in code and "plt.close('all')" not in code:
-            code = code.replace("plt.show()", "plt.show()\nplt.close('all')")
+        if "plt.show()" in code:
+            if "plt.close('all')" not in code:
+                code = code.replace("plt.show()", "plt.show()\nplt.close('all')")
+
+            if not self._is_running_in_console():
+                code = code.replace("plt.show()", "plt.show(block=False)")
         return code
