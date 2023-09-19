6d6984081b64e4c9568dacd2a865861e86cf9bf9
ArslanSaleem <khan.arslan38@gmail.com>
2023-09-19 13:34:37 +0500

    fix: missing , in where clause of example

--- examples/from_snowflake.py
+++ examples/from_snowflake.py
@@ -9,18 +9,18 @@ snowflake_connector = SnowFlakeSQLConnector(config={
         "Account": "ehxzojy-ue47135", 
         "database": "SNOWFLAKE_SAMPLE_DATA",
         "username": "test",
-        "password": "****",
+        "password": "*****",
         "table": "lineitem",
         "warehouse" : "COMPUTE_WH",
         "dbSchema": "tpch_sf1",
         "where": [
             # this is optional and filters the data to
             # reduce the size of the dataframe
-            ["l_quantity", ">" "49"]
+            ["l_quantity", ">","49"]
         ],
     })
 
-OPEN_AI_API = "Your-Api-Key"
+OPEN_AI_API = "Your-API-Key"
 llm = OpenAI(api_token=OPEN_AI_API)
 df = SmartDatalake([snowflake_connector], config={"llm": llm})
 
