7f17af85ccae24e37e405fa3b8642b7c4c9f36ad
ArslanSaleem <khan.arslan38@gmail.com>
2023-09-23 01:39:27 +0500

    fix: prompt comment

--- pandasai/prompts/explain_prompt.py
+++ pandasai/prompts/explain_prompt.py
@@ -1,10 +1,20 @@
 """ Prompt to explain solution generated
-Based on the last conversation you generated the code. 
-Can you explain briefly for non technical person on how you came up with code 
-without explaining pandas library?
+The previous conversation we had
+
+<Conversation>
+{conversation}
+</Conversation>
+
+Based on the last conversation you generated the following code:
+
+<Code>
+{code}
+</Code
+
+Explain how you came up with code for non-technical people without 
+mentioning technical details or mentioning the libraries used?
+
 """
-
-
 from .base import Prompt
 
 
