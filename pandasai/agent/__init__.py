adfc86a6fd5a606b25b8d6400f3012c11429f599
ArslanSaleem <khan.arslan38@gmail.com>
2023-09-25 14:57:44 +0500

    chore(agent): add max retries on queries

--- pandasai/agent/__init__.py
+++ pandasai/agent/__init__.py
@@ -3,6 +3,7 @@ from typing import Union, List, Optional
 from pandasai.helpers.df_info import DataFrameType
 from pandasai.helpers.logger import Logger
 from pandasai.helpers.memory import Memory
+from pandasai.prompts.base import Prompt
 from pandasai.prompts.clarification_questions_prompt import ClarificationQuestionPrompt
 from pandasai.prompts.explain_prompt import ExplainPrompt
 from pandasai.schemas.df_config import Config
@@ -38,6 +39,24 @@ class Agent:
         self._lake = SmartDatalake(dfs, config, logger, memory=Memory(memory_size))
         self._logger = self._lake.logger
 
+    def _call_llm_with_prompt(self, prompt: Prompt):
+        """
+        Call LLM with prompt using error handling to retry based on config
+        Args:
+            prompt (Prompt): Prompt to pass to LLM's
+        """
+        retry_count = 0
+        while retry_count < self._lake.config.max_retries:
+            try:
+                return self._lake.llm.call(prompt)
+            except Exception:
+                if (
+                    not self._lake.use_error_correction_framework
+                    or retry_count >= self._lake.config.max_retries - 1
+                ):
+                    raise
+                retry_count += 1
+
     def chat(self, query: str, output_type: Optional[str] = None):
         """
         Simulate a chat interaction with the assistant on Dataframe.
@@ -60,7 +79,7 @@ class Agent:
             self._lake.dfs, self._lake._memory.get_conversation()
         )
 
-        result = self._lake.llm.call(prompt)
+        result = self._call_llm_with_prompt(prompt)
         self._logger.log(
             f"""Clarification Questions:  {result}
             """
@@ -83,7 +102,7 @@ class Agent:
                 self._lake._memory.get_conversation(),
                 self._lake.last_code_executed,
             )
-            response = self._lake.llm.call(prompt)
+            response = self._call_llm_with_prompt(prompt)
             self._logger.log(
                 f"""Explaination:  {response}
                 """
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -55,12 +55,16 @@ class TestAgent:
         )
 
     @pytest.fixture
-    def llm(self, output: Optional[str] = None):
+    def llm(self, output: Optional[str] = None) -> FakeLLM:
         return FakeLLM(output=output)
 
     @pytest.fixture
-    def config(self, llm: FakeLLM):
+    def config(self, llm: FakeLLM) -> dict:
         return {"llm": llm}
+
+    @pytest.fixture
+    def agent(self, sample_df: pd.DataFrame, config: dict) -> Agent:
+        return Agent(sample_df, config)
 
     def test_constructor(self, sample_df, config):
         agent = Agent(sample_df, config)
@@ -163,3 +167,60 @@ the person with the most money.
 It's like finding the person who has the most marbles in a game
         """
         )
+
+    def test_call_prompt_success(self, sample_df, config):
+        agent = Agent(sample_df, config, memory_size=10)
+        agent._lake.llm.call = Mock()
+        clarification_response = """
+What is expected Salary Increase?
+        """
+        agent._lake.llm.call.return_value = clarification_response
+        agent._call_llm_with_prompt("Test Prompt")
+        assert agent._lake.llm.call.call_count == 1
+
+    def test_call_prompt_max_retries_exceeds(self, sample_df, config):
+        agent = Agent(sample_df, config, memory_size=10)
+        agent._lake.llm.call = Mock()
+        agent._lake.llm.call.side_effect = Exception("Raise an exception")
+        with pytest.raises(Exception):
+            agent._call_llm_with_prompt("Test Prompt")
+
+        assert agent._lake.llm.call.call_count == 3
+
+    def test_call_prompt_max_retry_on_error(self, sample_df, config):
+        agent = Agent(sample_df, config, memory_size=10)
+        agent._lake.llm.call = Mock()
+        agent._lake.llm.call.side_effect = [Exception(), Exception(), "LLM Result"]
+        result = agent._call_llm_with_prompt("Test Prompt")
+        assert result == "LLM Result"
+        assert agent._lake.llm.call.call_count == 3
+
+    def test_call_prompt_max_retry_twice(self, sample_df, config):
+        agent = Agent(sample_df, config, memory_size=10)
+        agent._lake.llm.call = Mock()
+        agent._lake.llm.call.side_effect = [Exception(), "LLM Result"]
+        result = agent._call_llm_with_prompt("Test Prompt")
+        assert result == "LLM Result"
+        assert agent._lake.llm.call.call_count == 2
+
+    def test_call_llm_with_prompt_no_retry_on_error(self, agent: Agent):
+        # Test when LLM call raises an exception but retries are disabled
+
+        agent._lake.config.use_error_correction_framework = False
+        agent._lake.llm.call = Mock()
+        agent._lake.llm.call.side_effect = Exception()
+        with pytest.raises(Exception):
+            agent._call_llm_with_prompt("Test Prompt")
+
+        assert agent._lake.llm.call.call_count == 1
+
+    def test_call_llm_with_prompt_max_retries_check(self, agent: Agent):
+        # Test when LLM call raises an exception but retries are disabled
+
+        agent._lake.config.max_retries = 5
+        agent._lake.llm.call = Mock()
+        agent._lake.llm.call.side_effect = Exception()
+        with pytest.raises(Exception):
+            agent._call_llm_with_prompt("Test Prompt")
+
+        assert agent._lake.llm.call.call_count == 5
