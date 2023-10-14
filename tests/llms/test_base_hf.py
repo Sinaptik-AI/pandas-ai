eaa00c35f9afa3c96d18bda38d8c05874fd78be1
Nautics889 <cyberukr@gmail.com>
2023-10-15 00:22:50 +0300

    feat: raise exc when get error HTTP code (#646)
    * (fix): according changes in tests
    * (tests): add test case for handling error HTTP code

--- tests/llms/test_base_hf.py
+++ tests/llms/test_base_hf.py
@@ -3,6 +3,7 @@
 import pytest
 import requests
 
+from pandasai.exceptions import LLMResponseHTTPError
 from pandasai.llm.base import HuggingFaceLLM
 from pandasai.prompts import AbstractPrompt
 
@@ -13,6 +14,10 @@ class TestBaseHfLLM:
     @pytest.fixture
     def api_response(self):
         return [{"generated_text": "Some text"}]
+
+    @pytest.fixture
+    def api_response_401(self):
+        return {"error": "Authorization header is correct, but the token seems invalid"}
 
     @pytest.fixture
     def prompt(self):
@@ -32,6 +37,7 @@ class TestBaseHfLLM:
 
     def test_query(self, mocker, api_response):
         response_mock = mocker.Mock()
+        response_mock.status_code = 200
         response_mock.json.return_value = api_response
         mocker.patch("requests.post", return_value=response_mock)
 
@@ -50,6 +56,27 @@ class TestBaseHfLLM:
 
         # Check that the result is correct
         assert result == api_response[0]["generated_text"]
+
+    def test_query_http_error_401(self, mocker, api_response_401):
+        response_mock = mocker.Mock()
+        response_mock.status_code = 401
+        response_mock.json.return_value = api_response_401
+        mocker.patch("requests.post", return_value=response_mock)
+
+        llm = HuggingFaceLLM(api_token="test_token")
+        payload = {"inputs": "Some input text"}
+
+        with pytest.raises(LLMResponseHTTPError) as exc:
+            llm.query(payload)
+
+        assert api_response_401.get("error") in str(exc.value)
+
+        requests.post.assert_called_once_with(
+            llm._api_url,
+            headers={"Authorization": "Bearer test_token"},
+            json=payload,
+            timeout=60,
+        )
 
     def test_call(self, mocker, prompt):
         huggingface = HuggingFaceLLM(api_token="test_token")
