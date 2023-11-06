fa64a5acbc7d59e98ab7ff5ce47d1cd9230d0ce0
Gabriele Venturi <lele.venturi@gmail.com>
2023-11-06 23:50:29 +0100

    feat: add new OpenAI models

--- pandasai/llm/openai.py
+++ pandasai/llm/openai.py
@@ -38,10 +38,12 @@ class OpenAI(BaseOpenAI):
         "gpt-4-0613",
         "gpt-4-32k",
         "gpt-4-32k-0613",
+        "gpt-4-1106-preview",
         "gpt-3.5-turbo",
         "gpt-3.5-turbo-16k",
         "gpt-3.5-turbo-0613",
         "gpt-3.5-turbo-16k-0613",
+        "gpt-3.5-turbo-1106",
     ]
     _supported_completion_models = ["text-davinci-003", "gpt-3.5-turbo-instruct"]
 
