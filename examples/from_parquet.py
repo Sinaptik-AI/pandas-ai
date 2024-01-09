890b3c05ede746dcaa72fb7939f8a6835337db35
tanersemenn <0418>
2024-01-09 15:44:25 +0300

    update the structure of parquet read

--- examples/from_parquet.py
+++ examples/from_parquet.py
@@ -1,15 +1,11 @@
 """Example of using PandasAI with a Parquet file."""
 
-import pandas as pd
 from pandasai import SmartDataframe
 from pandasai.llm import OpenAI
 
-parquet_file_path = "examples/data/Loan payments data.parquet"
-df = pd.read_parquet(parquet_file_path)
-
 llm = OpenAI()
 
-smart_df_read = SmartDataframe(df=df, config={"llm": llm})
+smart_df_read = SmartDataframe(df="examples/data/Loan payments data.parquet", config={"llm": llm})
 response = smart_df_read.chat("How many loans are from men and have been paid off?")
 print(response)
 # Output: 247 loans have been paid off by men.
