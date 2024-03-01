1283b3b204871d999054463bd13ce4993cd5ef06
Gabriele Venturi <lele.venturi@gmail.com>
2024-03-01 08:43:56 -0800

    feat: add support for Gemini API

--- pandasai/llm/__init__.py
+++ pandasai/llm/__init__.py
@@ -2,6 +2,7 @@ from .azure_openai import AzureOpenAI
 from .base import LLM
 from .google_palm import GooglePalm
 from .google_vertexai import GoogleVertexAI
+from .google_gemini import GoogleGemini
 from .huggingface_text_gen import HuggingFaceTextGen
 from .langchain import LangchainLLM
 from .openai import OpenAI
@@ -12,6 +13,7 @@ __all__ = [
     "OpenAI",
     "GooglePalm",
     "GoogleVertexAI",
+    "GoogleGemini",
     "HuggingFaceTextGen",
     "LangchainLLM",
 ]
