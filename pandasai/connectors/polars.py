b1fba1f6a28b30a0ada7a43ad47c0a34a30d7716
Gabriele Venturi <lele.venturi@gmail.com>
2024-03-08 10:49:19 -0800

    fix: polars import

--- pandasai/connectors/polars.py
+++ pandasai/connectors/polars.py
@@ -2,9 +2,23 @@
 Polars connector class to handle csv, parquet, xlsx files and polars dataframes.
 """
 
-import hashlib
 from functools import cache, cached_property
-from typing import Union
+from typing import Any, Union, TYPE_CHECKING
+import pandas as pd
+
+# Use a conditional import for type checking
+if TYPE_CHECKING:
+    try:
+        import polars as pl
+
+        PolarsDataFrame = pl.DataFrame
+        PolarsSeries = pl.Series
+    except ImportError:
+        PolarsDataFrame = Any
+        PolarsSeries = Any
+else:
+    PolarsDataFrame = Any
+    PolarsSeries = Any
 
 from pydantic import BaseModel
 
@@ -19,7 +33,7 @@ class PolarsConnectorConfig(BaseModel):
     Polars Connector configuration.
     """
 
-    original_df: pl.DataFrame
+    original_df: PolarsDataFrame
 
     class Config:
         arbitrary_types_allowed = True
@@ -30,7 +44,7 @@ class PolarsConnector(BaseConnector):
     Polars connector class to handle csv, parquet, xlsx files and polars dataframes.
     """
 
-    pandas_df = pl.DataFrame
+    pandas_df = pd.DataFrame
     _logger: Logger = None
     _additional_filters: list[list[str]] = None
 
@@ -46,19 +60,11 @@ class PolarsConnector(BaseConnector):
             config (PolarsConnectorConfig): The configuration for the Polars connector.
         """
 
-        try:
-            import polars as pl
-        except ImportError as e:
-            raise ImportError(
-                "Could not import polars python package. "
-                "Please install it with `pip install polars`."
-            ) from e
-
         super().__init__(config, **kwargs)
 
         self._load_df(self.config.original_df)
 
-    def _load_df(self, df: Union[pl.DataFrame, pl.Series, str, dict]):
+    def _load_df(self, df: Union[PolarsDataFrame, PolarsSeries, str, dict]):
         """
         Load the dataframe from a file or polars dataframe.
 
@@ -99,17 +105,20 @@ class PolarsConnector(BaseConnector):
         return PolarsConnectorConfig(**config)
 
     @cache
-    def head(self, n: int = 5) -> pl.DataFrame:
+    def head(self, n: int = 5) -> PolarsDataFrame:
         """
         Return the head of the data source that the connector is connected to.
         This information is passed to the LLM to provide the schema of the
         data source.
         """
+        if self.pandas_df is None:
+            return None
+
         sampler = DataSampler(self.pandas_df)
         return sampler.sample(n)
 
     @cache
-    def execute(self) -> pl.DataFrame:
+    def execute(self) -> PolarsDataFrame:
         """
         Execute the given query on the data source that the connector is
         connected to.
@@ -122,7 +131,7 @@ class PolarsConnector(BaseConnector):
         Return the number of rows in the data source that the connector is
         connected to.
         """
-        return len(self.pandas_df)
+        return len(self.pandas_df) if self.pandas_df.index else 0
 
     @cached_property
     def columns_count(self):
@@ -130,7 +139,7 @@ class PolarsConnector(BaseConnector):
         Return the number of columns in the data source that the connector is
         connected to.
         """
-        return len(self.pandas_df.columns)
+        return len(self.pandas_df.columns) if self.pandas_df.columns else 0
 
     @property
     def column_hash(self):
@@ -138,6 +147,8 @@ class PolarsConnector(BaseConnector):
         Return the hash code that is unique to the columns of the data source
         that the connector is connected to.
         """
+        import hashlib
+
         columns_str = "".join(self.pandas_df.columns)
         hash_object = hashlib.sha256(columns_str.encode())
         return hash_object.hexdigest()
