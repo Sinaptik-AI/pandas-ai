a05d4a09f81728cba5801b010db7ae03f49bbecb
ArslanSaleem <khan.arslan38@gmail.com>
2024-03-08 15:00:00 +0100

    fix: cockroach db connector

--- pandasai/connectors/sql.py
+++ pandasai/connectors/sql.py
@@ -576,61 +576,9 @@ class PostgreSQLConnector(SQLConnector):
         Args:
             config (ConnectorConfig): The configuration for the PostgreSQL connector.
         """
-        config["dialect"] = "postgresql"
-        config["driver"] = "psycopg2"
+        if "dialect" not in config:
+            config["dialect"] = "postgresql"
 
-        if isinstance(config, dict):
-            postgresql_env_vars = {
-                "host": "POSTGRESQL_HOST",
-                "port": "POSTGRESQL_PORT",
-                "database": "POSTGRESQL_DATABASE",
-                "username": "POSTGRESQL_USERNAME",
-                "password": "POSTGRESQL_PASSWORD",
-            }
-            config = self._populate_config_from_env(config, postgresql_env_vars)
-
-        super().__init__(config, **kwargs)
-
-    @cache
-    def head(self, n: int = 5) -> pd.DataFrame:
-        """
-        Return the head of the data source that the connector is connected to.
-        This information is passed to the LLM to provide the schema of the data source.
-
-        Returns:
-            DataFrame: The head of the data source.
-        """
-
-        if self.logger:
-            self.logger.log(
-                f"Getting head of {self.config.table} "
-                f"using dialect {self.config.dialect}"
-            )
-
-        # Run a SQL query to get all the columns names and 5 random rows
-        query = self._build_query(limit=n, order="RANDOM()")
-
-        # Return the head of the data source
-        return pd.read_sql(query, self._connection)
-
-
-class CockroachDBConnector(SQLConnector):
-    """
-    PostgreSQL connectors are used to connect to PostgreSQL databases.
-    """
-
-    def __init__(
-        self,
-        config: Union[SQLConnectorConfig, dict],
-        **kwargs,
-    ):
-        """
-        Initialize the PostgreSQL connector with the given configuration.
-
-        Args:
-            config (ConnectorConfig): The configuration for the PostgreSQL connector.
-        """
-        config["dialect"] = "cockroachdb"
         config["driver"] = "psycopg2"
 
         if isinstance(config, dict):
