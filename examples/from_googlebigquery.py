bf3392f5277ea9f0b80444a0a0d1217fe5ca38d6
Arslan Saleem <khan.arslan38@gmail.com>
2024-03-09 05:58:55 +0500

    feat(google_bigquery): load google big query from base64 string of cr… (#1008)
    * feat(google_bigquery): load google big query from base64 string of credentials
    
    * tests: add test case for base64 string
    
    * fix: fix linter
    
    ---------
    
    Co-authored-by: Gabriele Venturi <lele.venturi@gmail.com>

--- examples/from_googlebigquery.py
+++ examples/from_googlebigquery.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 from pandasai import SmartDataframe
 
 # A license might be required for using Snowflake with PandasAI
@@ -9,17 +12,45 @@ from pandasai.llm import OpenAI
 # KEYFILE_PATH
 # PROJECT_ID
 
+
+# EXAMPLE 1
 bigquery_connectors = GoogleBigQueryConnector(
     config={
-        "credentials_path": "credentials.json",
+        "credentials_path": "/Users/arslan/Downloads/loan-project.json",
         "database": "loan_payments",
         "table": "loan_payments",
-        "projectID": "project_id",
+        "projectID": "loan-project",
         "where": [["Gender", "=", "female"]],
     }
 )
 
-llm = OpenAI("OPEN_AI_KEY")
+llm = OpenAI("OPEN-API_KEY")
+df = SmartDataframe(bigquery_connectors, config={"llm": llm})
+
+response = df.chat("How many rows are there in data ?")
+print(response)
+
+# EXAMPLE 2
+# initialize google big query using Base64 string
+with open("/Users/arslan/Downloads/loan-project.json", "r") as file:
+    json_data = json.load(file)
+
+# Convert JSON data to a string
+json_string = json.dumps(json_data, indent=2)
+encoded_bytes = base64.b64encode(json_string.encode("utf-8"))
+
+
+bigquery_connectors = GoogleBigQueryConnector(
+    config={
+        "credentials_base64": encoded_bytes,
+        "database": "loan_payments",
+        "table": "loan_payments",
+        "projectID": "loan-project",
+        "where": [["Gender", "=", "female"]],
+    }
+)
+
+llm = OpenAI("OPEN-API_KEY")
 df = SmartDataframe(bigquery_connectors, config={"llm": llm})
 
 response = df.chat("How many rows are there in data ?")
