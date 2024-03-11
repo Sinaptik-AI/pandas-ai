93cae2b0e550d967a6ce9a43b9a8cdbbf18b3a50
shijunx <shijunx@amazon.com>
2024-03-11 22:07:22 +0800

    Support Bedrock Claude

--- pandasai/llm/__init__.py
+++ pandasai/llm/__init__.py
@@ -8,6 +8,7 @@ from .google_vertexai import GoogleVertexAI
 from .huggingface_text_gen import HuggingFaceTextGen
 from .langchain import LangchainLLM
 from .openai import OpenAI
+from .bedrock_claude import BedrockClaude
 
 __all__ = [
     "LLM",
