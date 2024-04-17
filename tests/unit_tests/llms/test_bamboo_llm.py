6eddac3c68d614ca3199851ef83aa4f5b3601731
ArslanSaleem <khan.arslan38@gmail.com>
2024-04-17 18:19:28 +0200

    fix: raise error if api response is not 200

--- tests/unit_tests/llms/test_bamboo_llm.py
+++ tests/unit_tests/llms/test_bamboo_llm.py
@@ -1,8 +1,17 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
+from pandasai.exceptions import PandasAIApiCallError
 from pandasai.llm.bamboo_llm import BambooLLM
 from pandasai.prompts.base import BasePrompt
+
+
+class MockHttpResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+    def json(self):
+        return {"data": "Hello World", "message": "test"}
 
 
 class TestBambooLLM(unittest.TestCase):
@@ -37,3 +46,20 @@ class TestBambooLLM(unittest.TestCase):
                 "query": ["What is Chroma?", "How does it work?"],
             }
         }
+
+    @patch("pandasai.helpers.request.requests.request", autospec=True)
+    def test_status_code_200(self, mock_request_lib):
+        prompt = self.get_prompt()
+        context = self.get_context()
+        bllm = BambooLLM(api_key="dummy_key")
+        mock_request_lib.return_value = MockHttpResponse(200)
+        bllm.call(prompt, context)
+
+    @patch("pandasai.helpers.request.requests.request", autospec=True)
+    def test_status_code_not_200(self, mock_request_lib):
+        prompt = self.get_prompt()
+        context = self.get_context()
+        bllm = BambooLLM(api_key="dummy_key")
+        mock_request_lib.return_value = MockHttpResponse(400)
+        with self.assertRaises(PandasAIApiCallError):
+            bllm.call(prompt, context)
