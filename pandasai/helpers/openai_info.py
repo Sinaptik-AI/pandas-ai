a6071a78ed6eb92d073fad1cb094d59ba3a3af6a
nehcneb <nehc.neb@gmail.com>
2024-05-15 18:54:44 +1000

    Update openai_info.py

--- pandasai/helpers/openai_info.py
+++ pandasai/helpers/openai_info.py
@@ -86,8 +86,7 @@ def standardize_model_name(
     if "ft:" in model_name:
         model_name = model_name.split(":")[1] + "-finetuned"
     if is_completion and (
-        #model_name.startswith("gpt-4") #Original
-        ((model_name.startswith("gpt-4")) and (not model_name.startswith("gpt-4o")))  #My amendment to remove '-completion' from GPT-4o
+        model_name.startswith("gpt-4")
         or model_name.startswith("gpt-3.5")
         or model_name.startswith("gpt-35")
         or "finetuned" in model_name
