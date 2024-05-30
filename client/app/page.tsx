0c32bce25b15f87615a4e3bcf7dfc15512c17caf
Arslan Saleem <khan.arslan38@gmail.com>
2024-05-30 17:11:30 +0200

    Revert "Release/v2.2: client folder added for frontend application"

--- client/app/page.tsx
+++
@@ -1,4 +0,0 @@
-import { redirect } from "next/navigation";
-export default function Home() {
-  redirect("/auth/sign-in");
-}
--- client/contexts/ContextProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { ConversationsProvider } from "./ConversationsProvider";
-
-const ContextProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ConversationsProvider>{children}</ConversationsProvider>;
-};
-
-export default ContextProvider;
