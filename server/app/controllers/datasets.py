dbae6b6b6ac617e8d91bc5587512aae886b63a5c
Shahrukh Khan <srkbannu@gmail.com>
2024-06-05 17:38:47 +0500

    workspace datasets endpoint added

--- server/app/controllers/datasets.py
+++ server/app/controllers/datasets.py
@@ -1,17 +1,21 @@
+from fastapi import status, HTTPException
 from app.models import Dataset
-from app.repositories import DatasetRepository
+from app.repositories import DatasetRepository, WorkspaceRepository
 from core.controller import BaseController
 from core.exceptions.base import NotFoundException
-
-
+from app.schemas.responses.datasets import WorkspaceDatasetsResponseModel
+from typing import List
 class DatasetController(BaseController[Dataset]):
     def __init__(
-        self, dataset_repository: DatasetRepository
+        self, 
+        dataset_repository: DatasetRepository,
+        space_repository: WorkspaceRepository
     ):
         super().__init__(model=Dataset, repository=dataset_repository)
         self.dataset_repository = dataset_repository
+        self.space_repository = space_repository
 
-    async def get_all_datasets(self):
+    async def get_all_datasets(self) -> WorkspaceDatasetsResponseModel:
         datasets = await self.get_all()
 
         if not datasets:
@@ -19,4 +23,4 @@ class DatasetController(BaseController[Dataset]):
                 "No dataset found. Please restart the server and try again"
             )
 
-        return {"data": datasets}
+        return WorkspaceDatasetsResponseModel(datasets=datasets)
