f8457fb35f6f1e57a64c94cebef4e46c42a07fef
Shahrukh Khan <srkbannu@gmail.com>
2024-06-07 15:40:15 +0500

    docker fixed

--- server/startup.sh
+++ server/startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Function to log messages
-log() {
-    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1"
-}
 
 # Load environment variables from .env file if it exists
 if [ -f .env ]; then
@@ -13,28 +9,17 @@ else
     log ".env file not found, skipping"
 fi
 
-# Echo the Python path
-log "Python path before activating Poetry environment: $(which python)"
-
-# Activate the Poetry environment
-log "Activating Poetry environment"
 source $(poetry env info --path)/bin/activate
 
-# Echo the Python path after activating Poetry environment
-log "Python path after activating Poetry environment: $(which python)"
 
-# Update Poetry lock file
-log "Updating Poetry lock file"
 poetry lock --no-update
 
-# Install dependencies
-log "Installing dependencies"
 make install
 
+/bin/sh wait-for-it.sh
+
 # Run database migrations
-log "Running database migrations"
 make migrate
 
 # Start the server in the background
-log "Starting the server"
 make start
