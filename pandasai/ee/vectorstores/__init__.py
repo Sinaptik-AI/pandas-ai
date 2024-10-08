0458de5204f1c42b0ae56b67579352c33ef26ad2
Harica404 <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-10-08 16:00:40 -0700

    add tests

--- pandasai/ee/vectorstores/__init__.py
+++ pandasai/ee/vectorstores/__init__.py
@@ -5,5 +5,6 @@ Vector stores to store data for training purpose
 from .chroma import ChromaDB
 from .lanceDB import LanceDB
 from .qdrant import Qdrant
+from .milvus import Milvus
 
-__all__ = ["ChromaDB", "Qdrant", "LanceDB"]
+__all__ = ["ChromaDB", "Qdrant", "LanceDB", "Milvus"]
