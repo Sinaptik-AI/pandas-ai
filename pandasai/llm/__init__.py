24c2a7606123a884827b4ae9cd6f61269b5f4164
Gabriele Venturi <lele.venturi@gmail.com>
2024-10-18 00:16:54 +0200

    refactor: make openai extension

--- pandasai/llm/__init__.py
+++ pandasai/llm/__init__.py
@@ -2,7 +2,6 @@ from .bamboo_llm import BambooLLM
 from .base import LLM
 from .bedrock_claude import BedrockClaude
 from .google_gemini import GoogleGemini
-from .google_palm import GooglePalm
 from .google_vertexai import GoogleVertexAI
 from .huggingface_text_gen import HuggingFaceTextGen
 from .ibm_watsonx import IBMwatsonx
@@ -11,7 +10,6 @@ from .langchain import LangchainLLM
 __all__ = [
     "LLM",
     "BambooLLM",
-    "GooglePalm",
     "GoogleVertexAI",
     "GoogleGemini",
     "HuggingFaceTextGen",
