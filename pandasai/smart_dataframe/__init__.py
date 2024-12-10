b6a56f2d2f9817c5a8debd1f236e9d2ed3efd41f
gdcsinaptik <giuseppe@sinaptik.ai>
2024-12-10 15:18:37 +0100

    fix typo in smart_dataframe breaking import

--- pandasai/smart_dataframe/__init__.py
+++ pandasai/smart_dataframe/__init__.py
@@ -29,7 +29,7 @@ class SmartDataframe:
             + "*" * 80
             + "\n"
             + "\033[1;33mDEPRECATION WARNING:\033[0m\n"
-            SmartDataframe will soon be deprecated. Please use df.chat() instead.
+            + "SmartDataframe will soon be deprecated. Please use df.chat() instead.\n"
             + "*" * 80
             + "\n",
             DeprecationWarning,
