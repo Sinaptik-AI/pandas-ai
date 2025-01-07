3cb822c1e5e1edc7d8a6d2747c52c535bf1fae57
ArslanSaleem <khan.arslan38@gmail.com>
2025-01-07 10:45:59 +0100

    fix: ruff errors

--- pandasai/llm/__init__.py
+++ pandasai/llm/__init__.py
@@ -8,8 +8,8 @@ from .google_vertexai import GoogleVertexAI
 from .huggingface_text_gen import HuggingFaceTextGen
 from .ibm_watsonx import IBMwatsonx
 from .langchain import LangchainLLM
-from .openai import OpenAI
 from .local_llm import LocalLLM
+from .openai import OpenAI
 
 __all__ = [
     "LLM",
