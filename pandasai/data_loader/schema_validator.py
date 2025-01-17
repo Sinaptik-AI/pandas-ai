b032812a7fb43e059fa9fdc7795a8efead091657
Raoul <scaliseraoul00@gmail.com>
2025-01-17 12:25:58 +0100

    fix(Agent): fixing chatting with multiple local dataframes

--- pandasai/data_loader/schema_validator.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def is_schema_source_same(schema1: dict, schema2: dict) -> bool:
-    return schema1.get("source").get("type") == schema2.get("source").get(
-        "type"
-    ) and json.dumps(
-        schema1.get("source").get("connection"), sort_keys=True
-    ) == json.dumps(schema2.get("source").get("connection"), sort_keys=True)
