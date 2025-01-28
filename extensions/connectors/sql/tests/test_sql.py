65dd727194925725317d8267e9c103347e802603
ArslanSaleem <khan.arslan38@gmail.com>
2025-01-28 13:37:56 +0100

    fix(sql): add params args to pass to the connector

--- extensions/connectors/sql/tests/test_sql.py
+++ extensions/connectors/sql/tests/test_sql.py
@@ -51,7 +51,7 @@ class TestDatabaseLoader(unittest.TestCase):
             database="test_db",
             port=3306,
         )
-        mock_read_sql.assert_called_once_with(query, mock_conn)
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=None)
 
         # Assert the result is a DataFrame
         self.assertIsInstance(result, pd.DataFrame)
@@ -89,7 +89,7 @@ class TestDatabaseLoader(unittest.TestCase):
             dbname="test_db",
             port=5432,
         )
-        mock_read_sql.assert_called_once_with(query, mock_conn)
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=None)
 
         # Assert the result is a DataFrame
         self.assertIsInstance(result, pd.DataFrame)
@@ -115,7 +115,7 @@ class TestDatabaseLoader(unittest.TestCase):
 
         # Assert that the connection is made and SQL query is executed
         mock_sqlite3_connect.assert_called_once_with("test_db.sqlite")
-        mock_read_sql.assert_called_once_with(query, mock_conn)
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=None)
 
         # Assert the result is a DataFrame
         self.assertIsInstance(result, pd.DataFrame)
@@ -153,9 +153,139 @@ class TestDatabaseLoader(unittest.TestCase):
             dbname="test_db",
             port=26257,
         )
-        mock_read_sql.assert_called_once_with(query, mock_conn)
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=None)
 
         # Assert the result is a DataFrame
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertEqual(result.shape, (2, 2))
+
+    @patch("pymysql.connect")
+    @patch("pandas.read_sql")
+    def test_load_from_mysql_with_params(self, mock_read_sql, mock_pymysql_connect):
+        mock_conn = MagicMock()
+        mock_pymysql_connect.return_value = mock_conn
+        mock_read_sql.return_value = pd.DataFrame(
+            {"column1": [1, 2], "column2": [3, 4]}
+        )
+
+        connection_info = {
+            "host": "localhost",
+            "user": "root",
+            "password": "password",
+            "database": "test_db",
+            "port": 3306,
+        }
+        query = "SELECT * FROM test_table WHERE id = %s"
+        query_params = [123]
+
+        connection_config = SQLConnectionConfig(**connection_info)
+
+        result = load_from_mysql(connection_config, query, query_params)
+
+        mock_pymysql_connect.assert_called_once_with(
+            host="localhost",
+            user="root",
+            password="password",
+            database="test_db",
+            port=3306,
+        )
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=query_params)
+
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertEqual(result.shape, (2, 2))
+
+    @patch("psycopg2.connect")
+    @patch("pandas.read_sql")
+    def test_load_from_postgres_with_params(self, mock_read_sql, mock_psycopg2_connect):
+        mock_conn = MagicMock()
+        mock_psycopg2_connect.return_value = mock_conn
+        mock_read_sql.return_value = pd.DataFrame(
+            {"column1": [5, 6], "column2": [7, 8]}
+        )
+
+        connection_info = {
+            "host": "localhost",
+            "user": "postgres",
+            "password": "password",
+            "database": "test_db",
+            "port": 5432,
+        }
+        query = "SELECT * FROM test_table WHERE name ILIKE %s"
+        query_params = ["%John%"]
+
+        connection_config = SQLConnectionConfig(**connection_info)
+
+        result = load_from_postgres(connection_config, query, query_params)
+
+        mock_psycopg2_connect.assert_called_once_with(
+            host="localhost",
+            user="postgres",
+            password="password",
+            dbname="test_db",
+            port=5432,
+        )
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=query_params)
+
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertEqual(result.shape, (2, 2))
+
+    @patch("sqlite3.connect")
+    @patch("pandas.read_sql")
+    def test_load_from_sqlite_with_params(self, mock_read_sql, mock_sqlite3_connect):
+        mock_conn = MagicMock()
+        mock_sqlite3_connect.return_value = mock_conn
+        mock_read_sql.return_value = pd.DataFrame(
+            {"column1": [9, 10], "column2": [11, 12]}
+        )
+
+        connection_info = {"file_path": "test_db.sqlite"}
+        query = "SELECT * FROM test_table WHERE age > ?"
+        query_params = [30]
+
+        connection_config = SqliteConnectionConfig(**connection_info)
+
+        result = load_from_sqlite(connection_config, query, query_params)
+
+        mock_sqlite3_connect.assert_called_once_with("test_db.sqlite")
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=query_params)
+
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertEqual(result.shape, (2, 2))
+
+    @patch("psycopg2.connect")
+    @patch("pandas.read_sql")
+    def test_load_from_cockroachdb_with_params(
+        self, mock_read_sql, mock_postgresql_connect
+    ):
+        mock_conn = MagicMock()
+        mock_postgresql_connect.return_value = mock_conn
+        mock_read_sql.return_value = pd.DataFrame(
+            {"column1": [13, 14], "column2": [15, 16]}
+        )
+
+        connection_info = {
+            "host": "localhost",
+            "user": "root",
+            "password": "password",
+            "database": "test_db",
+            "port": 26257,
+        }
+        query = "SELECT * FROM test_table WHERE status = %s"
+        query_params = ["active"]
+
+        connection_config = SQLConnectionConfig(**connection_info)
+
+        result = load_from_cockroachdb(connection_config, query, query_params)
+
+        mock_postgresql_connect.assert_called_once_with(
+            host="localhost",
+            user="root",
+            password="password",
+            dbname="test_db",
+            port=26257,
+        )
+        mock_read_sql.assert_called_once_with(query, mock_conn, params=query_params)
+
         self.assertIsInstance(result, pd.DataFrame)
         self.assertEqual(result.shape, (2, 2))
 
